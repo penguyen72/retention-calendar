84be41933657f53ae3926e2038d5811c0d34d2a4
penguyen72 <peytonn72@gmail.com>
2025-02-02 14:46:40 -0800

    add logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,80 @@
+import { AddGoalButton, DataType } from "@/components/add-goal-button"
+import { Calendar } from "@/components/ui/calendar"
+import { Card, CardHeader, CardTitle } from "@/components/ui/card"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu"
+import { isSameDay } from "date-fns"
+import { Ellipsis, Trash } from "lucide-react"
 import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
+import { useLocalStorage } from "usehooks-ts"
+import { Button } from "./components/ui/button"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [open, setOpen] = useState(false)
+  const [date, setDate] = useState<Date>(new Date())
+  const [database, setDatabase] = useLocalStorage<DataType[]>(
+    "retention-calender",
+    []
+  )
+
+  const dataSource = database.filter((item) => isSameDay(item.date, date))
+
+  function deleteEvent(id: string) {
+    setDatabase((prevValue) => {
+      return prevValue.filter((item) => item.id !== id)
+    })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col h-full gap-4 p-8">
+      <p className="text-3xl font-bold text-primary">Retention Calendar</p>
+      <div className="grid h-full grid-cols-1 p-8 border rounded-md lg:grid-cols-2">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={(date) => date && setDate(date)}
+          className="m-auto scale-125 border rounded-md shadow"
+        />
+        <div className="flex flex-col gap-4">
+          <AddGoalButton selectedDate={date} open={open} setOpen={setOpen} />
+          <div className="flex flex-col gap-4 overflow-y-scroll">
+            {dataSource.map((item, index) => {
+              return (
+                <Card key={index}>
+                  <CardHeader>
+                    <CardTitle className="flex items-center justify-between">
+                      {item.name}
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button size="icon" variant="ghost" type="button">
+                            <Ellipsis />
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-56">
+                          <DropdownMenuItem
+                            className="!text-destructive hover:!bg-destructive/10"
+                            onClick={() => {
+                              deleteEvent(item.id)
+                            }}
+                          >
+                            <Trash />
+                            <span>Delete</span>
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </CardTitle>
+                  </CardHeader>
+                </Card>
+              )
+            })}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/ui/calendar.tsx
@@ -0,0 +1,96 @@
+import * as React from "react"
+import { ChevronLeft, ChevronRight } from "lucide-react"
+import { Day, DayPicker } from "react-day-picker"
+
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+import { useLocalStorage } from "usehooks-ts"
+import { DataType } from "@/components/add-goal-button"
+import { isSameDay } from "date-fns"
+
+export type CalendarProps = React.ComponentProps<typeof DayPicker>
+
+function Calendar({
+  className,
+  classNames,
+  showOutsideDays = true,
+  ...props
+}: CalendarProps) {
+  const [database, setDatabase] = useLocalStorage<DataType[]>(
+    "retention-calender",
+    []
+  )
+
+  return (
+    <DayPicker
+      showOutsideDays={showOutsideDays}
+      className={cn("p-3", className)}
+      classNames={{
+        months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
+        month: "space-y-4",
+        caption: "flex justify-center pt-1 relative items-center",
+        caption_label: "text-sm font-medium",
+        nav: "space-x-1 flex items-center",
+        nav_button: cn(
+          buttonVariants({ variant: "outline" }),
+          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
+        ),
+        nav_button_previous: "absolute left-1",
+        nav_button_next: "absolute right-1",
+        table: "w-full border-collapse space-y-1",
+        head_row: "flex",
+        head_cell:
+          "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]",
+        row: "flex w-full mt-2",
+        cell: cn(
+          "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-end)]:rounded-r-md",
+          props.mode === "range"
+            ? "[&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md"
+            : "[&:has([aria-selected])]:rounded-md"
+        ),
+        day: cn(
+          buttonVariants({ variant: "ghost" }),
+          "h-8 w-8 p-0 font-normal aria-selected:opacity-100"
+        ),
+        day_range_start: "day-range-start",
+        day_range_end: "day-range-end",
+        day_selected:
+          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
+        day_today: "bg-accent text-accent-foreground",
+        day_outside:
+          "day-outside text-muted-foreground aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
+        day_disabled: "text-muted-foreground opacity-50",
+        day_range_middle:
+          "aria-selected:bg-accent aria-selected:text-accent-foreground",
+        day_hidden: "invisible",
+        ...classNames
+      }}
+      components={{
+        IconLeft: ({ className, ...props }) => (
+          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+        ),
+        IconRight: ({ className, ...props }) => (
+          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+        ),
+        Day: (props) => {
+          const dataSource = database.filter((item) =>
+            isSameDay(item.date, props.date)
+          )
+
+          return (
+            <div className="relative">
+              {dataSource.length > 0 ? (
+                <span className="absolute top-0.5 right-0.5 grid min-h-1 min-w-1 translate-x-1/2 -translate-y-1/2 rounded-full bg-red-600 py-1 px-1 text-xs text-white"></span>
+              ) : null}
+              <Day {...props} />
+            </div>
+          )
+        }
+      }}
+      {...props}
+    />
+  )
+}
+Calendar.displayName = "Calendar"
+
+export { Calendar }
