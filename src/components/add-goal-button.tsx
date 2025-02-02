433703d4c3a13024abf29575a725b577071517c5
penguyen72 <peytonn72@gmail.com>
2025-02-02 15:14:53 -0800

    remove unused vars check

--- src/components/add-goal-button.tsx
+++ src/components/add-goal-button.tsx
@@ -53,7 +53,7 @@ export interface DataType {
 }
 
 export function AddGoalButton({ selectedDate, open, setOpen }: Props) {
-  const [database, setDatabase] = useLocalStorage<DataType[]>(
+  const [_database, setDatabase] = useLocalStorage<DataType[]>(
     "retention-calender",
     []
   )
